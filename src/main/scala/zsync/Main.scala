f242593bb631269d703ad32ca31ede7e686b696d
yatsukha <yatsukha.roman@gmail.com>
2020-09-05 17:19:09 +0100

    Adjust Main

--- src/main/scala/zsync/Main.scala
+++ src/main/scala/zsync/Main.scala
@@ -2,20 +2,16 @@ package zsync
 
 import zio._
 import zio.blocking.Blocking
-import console._
-import nio.file.Files
+import console.{putStr => _, putStrErr => _, _}
 import zsync.context._
 import zsync.config._
-import zsync.app.Add
-import zsync.app.Remove
-import zsync.app.Backup
-import zsync.app.Help
-import zsync.app.Action
+import zsync.app._
 
 object Main extends zio.App {
 
   // today's TODO:
-  //  - add git backup
+  //  - add handling of existing files
+  //  - refactor zip so that it uses a stream of ZManaged
 
   def program(
     args: List[String]
@@ -28,26 +24,15 @@ object Main extends zio.App {
         _   => ZIO.unit
       )
 
-  def printCommand(
-    args: List[String]
-  ): ZIO[Console with Blocking with Context, Nothing, Unit] =
-    Action
-      .parse(args)
-      .foldM(
-        err => putStrErr(err),
-        cmd => putStrLn(cmd.toString())
-      )
-
-  lazy val bootstrap =
-    Context.live ++ Console.live ++
-      ((Context.live ++ Blocking.live) >>> Config.live)
+  lazy val dependencies =
+    Context.live ++ ((Context.live ++ Blocking.live) >>> Config.live)
 
   override def run(args: List[String]): URIO[ZEnv, ExitCode] =
     program(args)
-      .provideCustomLayer(bootstrap)
+      .provideCustomLayer(dependencies)
       .foldM(
-        err => putStrErr(s"Unrecoverable error: $err").as(ExitCode(1)),
-        _   => ZIO.unit.as(ExitCode(0))
+        err => putStrLnErr(s"Unrecoverable error: $err").as(ExitCode(1)),
+        _   => ZIO.succeed(ExitCode(0))
       )
 
 }
